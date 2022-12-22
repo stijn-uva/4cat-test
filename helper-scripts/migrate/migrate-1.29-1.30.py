a7902f59df615a3d88ad6e05a8823bdc1f51c54f
Stijn Peeters <42036349+stijn-uva@users.noreply.github.com>
2022-12-22 16:21:31 +0100

    dsfsdfds

--- helper-scripts/migrate/migrate-1.29-1.30.py
+++ helper-scripts/migrate/migrate-1.29-1.30.py
@@ -98,7 +98,8 @@ else:
     in_docker = False
     config_path = Path(__file__).parent.parent.parent.joinpath("config/config.ini")
     if config_path.exists():
-        config_reader = configparser.ConfigParser().read(config_path)
+        config_reader = configparser.ConfigParser()
+        config_reader.read(config_path)
         in_docker = config_reader["DOCKER"].getboolean("use_docker_config")
         print("yes" if in_docker else "no")
     else:
